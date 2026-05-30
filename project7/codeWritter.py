380df7300f061be658c9206b7ba61ec08c01f165
denizdalbasi <denizdalbasi2007@gmail.com>
2026-05-30 19:00:49 +0300

    resolve typos and syntax errors in parser and codewriter

--- project7/codeWritter.py
+++ project7/codeWritter.py
@@ -2,6 +2,7 @@ class codeWritter:
     def __init__(self, output_path):
         self.file = open(output_path, 'w')
         self.filename = output_path.split('/')[-1].replace('.asm','')
+        self.label_counter = 0  # eq, gt, lt için sayaç ekledim
 
         self.segments = {
             "local": "LCL",
@@ -19,10 +20,11 @@ class codeWritter:
                 "@SP", "AM=M-1","D=M",
                 "@SP", "A=M-1"
             ])
-            if command == 'add':asm.append("M=D+M")
-            elif command == 'sub':asm.append("M=M-D")
-            elif command == 'and':asm.append("M=D&M")
-            elif command == 'or':asm.append("M=D|M")
+            if command == 'add': asm.append("M=D+M")
+            elif command == 'sub': asm.append("M=M-D")
+            elif command == 'and': asm.append("M=D&M")
+            elif command == 'or': asm.append("M=D|M")
+            
         elif command in ["neg", "not"]:
             asm.extend([ "@SP", "A=M-1"])
             if command == 'neg': asm.append("M=-M")
@@ -37,12 +39,17 @@ class codeWritter:
             
             asm.extend([
                 "@SP","AM=M-1","D=M",
-                "@SP", "A=M_1", "D=M-D",
-                f"@{label_true}", f"D{jmp_type}",
-                f"@{label_end}", "0;JMP",
-                f"{label_true}",
+                "@SP", "A=M-1", "D=M-D",        # A=M_1 yerine A=M-1 yaptim
+                f"@{label_true}", f"D;{jmp_type}", # Araya semikolon (;) eklendi
+                "@SP", "A=M-1", "M=0",          # Koşulun yanlış (FALSE) olma durumu ekledim
+                f"@{label_end}", "0;JMP",       #Yanlışsa TRUE bloğuna girmeden sona atlasın
+                f"({label_true})",              #  Etiket tanımına parantez ekledim
                 "@SP", "A=M-1","M=-1",
                 f"({label_end})"
             ])
 
-            self.file.write("\n".join(asm)+"\n")
+        self.file.write("\n".join(asm)+"\n")
+        
+    def write_push_pop(self, command_type, segment, index):
+        pass
+
